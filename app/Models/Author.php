c862f529ed2bd9a99fe86d17e4af010663f6f6a7
Wojciech Gawronski <wojciech@gawronsky.com.pl>
2021-05-16 18:36:38 +0200

    add authors test and code

---
+++ app/Models/Author.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+    // colums in database to custom authomaticly by Laravel Carbon class
+    protected $dates = ['birth'];
+
+    /**
+     * @param string $birth day of birtd
+     */
+    public function setBirthAttribute(string $birth)
+    {
+        $this->attributes['birth'] = Carbon::parse($birth);
+    }
+}
---
+++ tests/Feature/AuthorManagmentTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Author;
+
+class AuthorManagmentTest extends TestCase
+{
+    // It is often useful to reset your database after each test so that data from a previous test does not interfere with subsequent tests.
+    // It takes the most optimal approach to migrating your test database depending on if you are using an in-memory database or a traditional database.
+    // Use the trait on your test class and everything will be handled for you:
+    use RefreshDatabase;
+
+    /** @test  */
+    public function an_author_can_be_created()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->post('/author', [
+            'name' => 'author name',
+            'birth' => '05/11/1986',
+        ]);
+
+        $author = Author::all();
+
+        $this->assertCount(1, $author);
+        // Carbon - A simple PHP API extension for DateTime.
+        $this->assertInstanceOf(Carbon::class, $author->first()->birth);
+        // for confirm to correct date format:
+        $this->assertEquals('1986/11/05', $author->first()->birth->format('Y/d/m'));
+    }
+}
